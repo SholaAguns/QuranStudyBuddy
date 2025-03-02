cf3d9970d8889c140ff97f963b52265503d4ac24
SholaAguns <106911290+SholaAguns@users.noreply.github.com>
2025-03-02 06:25:21 +0000

    prod settings

--- qsbproject/settings/prod.py
+++ qsbproject/settings/prod.py
@@ -1,24 +1,19 @@
 from .base import *
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-DEBUG = False
-
-SECRET_KEY = os.getenv("QSB_SECRET_KEY")
 
 ALLOWED_HOSTS = []
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
     }
 }
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
-#STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles"
