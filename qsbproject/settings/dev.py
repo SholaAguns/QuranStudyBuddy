0a78f624f8511308a85336baf6071922d0257b76
SholaAguns <106911290+SholaAguns@users.noreply.github.com>
2025-03-02 06:19:30 +0000

    -Manage different envs

--- qsbproject/settings/dev.py
+++ qsbproject/settings/dev.py
@@ -11,5 +11,16 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": env("DB_NAME"),
+#         "USER": env("DB_USER"),
+#         "PASSWORD": env("DB_PASSWORD"),
+#         "HOST": env("DB_HOST"),
+#         "PORT": env("DB_PORT"),
+#     }
+# }
+
 STATICFILES_DIRS = [BASE_DIR / "static"]
 #STATIC_ROOT = BASE_DIR / "staticfiles"
