dca1c32fc8a0d1abf0584cbf83d14eec4e010d01
SholaAguns <106911290+SholaAguns@users.noreply.github.com>
2025-03-07 19:47:13 +0000

    phrase migrations

--- arabic/forms.py
+++ arabic/forms.py
@@ -5,10 +5,13 @@ class PhraseForm(forms.ModelForm):
 
     class Meta:
         model = Phrase
-        fields = ('text', 'translation', 'category', 'tags', 'image')
+        fields = ('text', 'translation', 'transliteration' ,'category', 'tags', 'image')
 
     def __init__(self, *args, **kwargs):
         super(PhraseForm, self).__init__(*args, **kwargs)
         self.fields['image'].required = False
         self.fields['tags'].required = False
         self.fields['category'].required = False
+        self.fields['transliteration'].required = False
+        self.fields['text'].label = 'Text (required)'
+        self.fields['translation'].label = 'Translation (required)'
--- arabic/models.py
+++ arabic/models.py
@@ -9,7 +9,8 @@ class Phrase(models.Model):
     text = models.CharField(max_length=300)
     category = models.CharField(max_length=60)
     tags = models.CharField(max_length=200, null=True)
-    translation = models.TextField(max_length=300)
+    translation = models.CharField(max_length=400)
+    transliteration = models.CharField(max_length=400, null=True)
     image = models.ImageField(upload_to="images", null=True)
     created_dt = models.DateTimeField(auto_now=True)
 
